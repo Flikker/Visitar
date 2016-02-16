8b884cb16118c4ba360f8c8ee1a7475d47f8a449
Flikker <contact@revolutioninteractivevideo.com>
2016-02-16 12:10:01 +0000

    Testing if p build is updating the app

--- www/js/searchtours.js
+++ www/js/searchtours.js
@@ -1,6 +1,6 @@
 // I've changed to pagebeforecreate as was multiplying results with pagebeforeshow. check if it refreshes list when reopen app
 /* this bit empties the current searchlist so it's not duplicated, then makes an ajax call to search.php for the list of available tours. The href takes people through to detailsPage which is in tourdetails.html, finding the relevant details from the .html?index= . */
-$(document).on('pageinit', '#searchpage', function(){
+$(document).on('pagebeforecreate', '#searchpage', function(){
     $('#listsearch').empty();
     var output = $('#listsearch');
     $.ajax({
@@ -29,7 +29,7 @@ $(document).on('pageinit', '#searchpage', function(){
 
 
 /* the code for detailsPage in tourdetails.html. Takes the id of the tour clicked in the search list which the code above passed into the detailspage html (getUrlVars), then id is passed to display.php to show the details of the correct tour. Problems included using 'index', which is a reserved term - changed to 'id'. Also had problems with correct syntax for get in display.php, and then forgot to re-add ?id= to end of php call. */
-$(document).on('pageinit', '#detailsPage', function(event) {
+$(document).on('pagebeforecreate', '#detailsPage', function(event) {
 	
     	var id = getUrlVars()["id"];
         
@@ -42,7 +42,7 @@ $(document).on('pageinit', '#detailsPage', function(event) {
         timeout: 10000,
         success: function(data, status){
             $.each(data, function(i,item){
-                var place = 'item.descrip';
+                var place = '<div class="searchimage"><img  src="' +item.picture+ '" alt="image" ><br><div class="picandtext"><h2 align="center">' +item.name+ '</h2><h4 align="center">' +item.address+ '</h4><p>' +item.descrip+ '</p></div><progress max="100" value="0" id="ft-prog" ></progress></div><div id="status" class="blink"></div><input class="buttonClass" id="download" type="button" value="Download ' +item.filesize+ '"  onclick1="' +item.ziploc+ '" onclick2="' +item.id+ '" onclick3="' +item.name+ '" onclick4="' +item.address+ '"   >';
                 
                 output.append(place);
                
