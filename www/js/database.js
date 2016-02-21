708cbc3a8f74c63a2deeccb4ed8bf8b66a9dde81
Flikker <contact@revolutioninteractivevideo.com>
2016-02-21 11:24:47 +0000

    Changed delete confirmation to notification.confirm; removed 2nd listview2refresh
    See if removing second listview2 refresh resolves issue of two tours
    appearing

--- www/js/database.js
+++ www/js/database.js
@@ -86,10 +86,6 @@ $('#downloadlist2').listview('refresh');
  
 
 
- $('#downloadlist2').listview('refresh');
-
-}
-
 
 
 
@@ -214,8 +210,8 @@ window.resolveLocalFileSystemURL(folderEntry, onSuccess, onError);
 // of newLoccy(the location on the UNZIPPED folder), the name, and the id of that particular listview element (dynamically gave each
 // listview element a different id by passing the database unique identifier into the <li id=
 function getRid(x, y, z) {
-    if(confirm("Delete " + y + "?") == true) {
-        
+    if  navigator.notification.confirm.buttonIndex("Are you sure you want to delete " + y + "?", confirmCallback, "Delete", "Delete it!, Keep it!") == 1 {
+       
         
         delFolder(x, y, z);
         
