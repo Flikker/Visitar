c027c4c69e2ff5d2369a24b73217c2620476b0b8
Flikker <contact@revolutioninteractivevideo.com>
2016-02-21 13:45:23 +0000

    reverted

--- www/js/database.js
+++ www/js/database.js
@@ -86,6 +86,9 @@ $('#downloadlist2').listview('refresh');
  
 
 
+ $('#downloadlist2').listview('refresh');
+
+}
 
 
 
@@ -177,7 +180,7 @@ function delFolder(folderEntry, namey, listEntry) {
     
     
     function successy(parent) {
-    alert(parent); alert(listEntry);
+   
      document.getElementById(listEntry).style.display = "none";
     delRecord(listEntry);
 }
