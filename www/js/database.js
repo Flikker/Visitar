12334148e6e5019b92ab0876f98e95b8394c20f4
Flikker <contact@revolutioninteractivevideo.com>
2016-02-25 11:08:13 +0000

    removed getpathy from showdb

--- www/js/database.js
+++ www/js/database.js
@@ -129,9 +129,7 @@ window.resolveLocalFileSystemURL(wholepathy, function(entry) {
 
 
 function showDB(tx) {
-    getPathy();
-    
- var db = window.openDatabase("Database", "1.0", "DEMO", 2000000);
+   var db = window.openDatabase("Database", "1.0", "DEMO", 2000000);
  db.transaction(createDB, errorCB, successCB);
 }
 function createDB(tx) {
