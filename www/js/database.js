b2b527a5f6454ee5694b2e743ef5b937ffb68e07
Flikker <contact@revolutioninteractivevideo.com>
2016-02-25 12:19:41 +0000

    put getpathy back above querysuccess

--- www/js/database.js
+++ www/js/database.js
@@ -61,7 +61,16 @@ function itDidnt() {
 }
 
 
- 
+ var hopeitwork;
+function getPathy() {
+    var wholepathy = 'cdvfile://localhost/persistent/';
+window.resolveLocalFileSystemURL(wholepathy, function(entry) {
+    hopeitwork = entry.toURL();
+    alert("this is in getpathy " + hopeitwork);
+    return hopeitwork;
+});
+}
+
 
 
 function querySuccess(tx, results) {
