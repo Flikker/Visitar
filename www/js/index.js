53717033145ebdb518a71eaea3ffb21f815a4cbb
Flikker <contact@revolutioninteractivevideo.com>
2016-07-10 14:25:13 +0100

    removed peter metz, added bluetoothle

--- www/js/index.js
+++ www/js/index.js
@@ -34,7 +34,7 @@ var app = {
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready'); getPathy();
-       
+        cordova.plugins.BluetoothStatus.initPlugin();
         
         
         
